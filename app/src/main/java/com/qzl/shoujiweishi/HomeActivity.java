e9b7e446263695a9a1519b5f94828c7304ab804a
qzl <2538096489@qq.com>
2016-06-04 18:13:59 +0800

    homeActivity主界面布局实现

--- app/src/main/java/com/qzl/shoujiweishi/HomeActivity.java
+++ app/src/main/java/com/qzl/shoujiweishi/HomeActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -22,6 +23,12 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private class Myadapter extends BaseAdapter{
+        //存放图片id
+        int[] imageId = { R.drawable.safe, R.drawable.callmsgsafe, R.drawable.app,
+                R.drawable.taskmanager, R.drawable.netmanager, R.drawable.trojan,
+                R.drawable.sysoptimize, R.drawable.atools, R.drawable.settings };
+        String[] names = { "手机防盗", "通讯卫士", "软件管理", "进程管理", "流量统计", "手机杀毒", "缓存清理",
+                "高级工具", "设置中心" };
 
         //设置条目的个数
         @Override
@@ -47,8 +54,14 @@ public class HomeActivity extends AppCompatActivity {
             /*TextView textView = new TextView(getApplicationContext());
             textView.setText("第"+position+"个条目");//position:代表是条目的位置，从0开始0-8*/
             //将布局文件转化成view对象
-            View.inflate(getApplicationContext(),)
-            return ;
+            View view = View.inflate(getApplicationContext(), R.layout.item_home, null);
+            //每个条目的样式都不一样，初始化控件，去设置控件的值
+            //view.findViewById ： 是从item_home布局文件中找控件，findViewById：是从activity_home中找控件了
+            ImageView imageView_itemhome_icon = (ImageView) view.findViewById(R.id.imageView_itemhome_icon);
+            TextView tv_itemhome_text = (TextView) view.findViewById(R.id.tv_itemhome_text);
+            imageView_itemhome_icon.setImageResource(imageId[position]);//给imageView设置图片,根据条目的位置从图片数组中获取相应的图片
+            tv_itemhome_text.setText(names[position]);
+            return view;
         }
     }
 }
