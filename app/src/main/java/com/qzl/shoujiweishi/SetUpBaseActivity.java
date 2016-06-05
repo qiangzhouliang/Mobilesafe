a92c28f9a53a7832705819434fe14faa6a0ea864
qzl <2538096489@qq.com>
2016-06-05 18:05:18 +0800

    抽取到父类的操作，以实现按返回键是跳到上一个界面

--- app/src/main/java/com/qzl/shoujiweishi/SetUpBaseActivity.java
+++ app/src/main/java/com/qzl/shoujiweishi/SetUpBaseActivity.java
@@ -3,6 +3,7 @@ package com.qzl.shoujiweishi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 
 public abstract class SetUpBaseActivity extends AppCompatActivity {
@@ -13,7 +14,7 @@ public abstract class SetUpBaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up_base);
     }
-    //按钮的操作
+    //按钮的点击事件操作
     public void pre(View view){
         pre_activity();
     }
@@ -25,4 +26,24 @@ public abstract class SetUpBaseActivity extends AppCompatActivity {
     public abstract void next_activity();
     //上一步操作
     public abstract void pre_activity();
+    //在父类中直接对子类中的返回键进行统一处理
+    /*@Override
+    public void onBackPressed() {
+        pre_activity();
+        super.onBackPressed();
+    }*/
+
+    //监听手机物理按键的点击事件
+    //keyCode : 表示物理按键的标识
+    //event ： 按键的处理事件
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //判断keycode是否是返回键的标识
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            //true 是可以屏蔽按键的，高版本的home键是不能屏蔽的
+            //return true;
+            pre_activity();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
