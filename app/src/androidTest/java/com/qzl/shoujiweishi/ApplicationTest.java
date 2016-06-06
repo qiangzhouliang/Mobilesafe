cbbfb656315697234144464170edd6f02b2b89b9
qzl <2538096489@qq.com>
2016-06-06 17:24:59 +0800

    获取联系人操作

--- app/src/androidTest/java/com/qzl/shoujiweishi/ApplicationTest.java
+++ app/src/androidTest/java/com/qzl/shoujiweishi/ApplicationTest.java
@@ -2,6 +2,12 @@ package com.qzl.shoujiweishi;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.widget.Toast;
+
+import com.qzl.shoujiweishi.engine.ContactEngine;
+
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +15,11 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+    public void testContacts(){
+        List<HashMap<String,String>> list = ContactEngine.getAllContactInfo(getContext());
+        for (HashMap<String, String> hashMap: list){
+            System.out.println("姓名："+hashMap.get("name")+"    电话"+hashMap.get("phone"));
+        }
     }
 }
