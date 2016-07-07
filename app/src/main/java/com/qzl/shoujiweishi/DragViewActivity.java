fd2b813df494982c7a1780f3547c2056915e650f
Qzl <2538096489@qq.com>
2016-07-07 17:15:29 +0800

    外拨电话和来电显示界面toast的拖动

--- app/src/main/java/com/qzl/shoujiweishi/DragViewActivity.java
+++ app/src/main/java/com/qzl/shoujiweishi/DragViewActivity.java
@@ -2,6 +2,7 @@ package com.qzl.shoujiweishi;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class DragViewActivity extends Activity {
 
@@ -17,6 +19,8 @@ public class DragViewActivity extends Activity {
     private SharedPreferences sp;
     private int width;
     private int height;
+    private TextView tv_dragview_top;
+    private TextView tv_dragview_bottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,8 @@ public class DragViewActivity extends Activity {
         sp = getSharedPreferences("config",MODE_PRIVATE);
         setContentView(R.layout.activity_drag_view);
         ll_dragview_toast = (LinearLayout) findViewById(R.id.ll_dragview_toast);
+        tv_dragview_top = (TextView) findViewById(R.id.tv_dragview_top);
+        tv_dragview_bottom = (TextView) findViewById(R.id.tv_dragview_bottom);
         //设置控件的回显操作
         //1.获取保存的左边
         int x = sp.getInt("x",0);
@@ -50,7 +56,51 @@ public class DragViewActivity extends Activity {
         windowManager.getDefaultDisplay().getMetrics(outMetrics);//给白纸设置宽高
         width = outMetrics.widthPixels;
         height = outMetrics.heightPixels;
+        //回显
+        if(y > height/2){
+            //隐藏下方显示上方
+            tv_dragview_bottom.setVisibility(View.INVISIBLE);
+            tv_dragview_top.setVisibility(View.VISIBLE);
+        }else {
+            //隐藏上方显下方
+            tv_dragview_bottom.setVisibility(View.VISIBLE);
+            tv_dragview_top.setVisibility(View.INVISIBLE);
+        }
         setTouch();
+        //双击居中
+        setDoubleClick();
+    }
+    long[] mHits = new long[2];//2表示双击
+    /**
+     * 双击居中事件
+     */
+    private void setDoubleClick() {
+        //点击事件 ： 按下 + 抬起
+        ll_dragview_toast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //拷贝数组的操作
+                //mHits : 拷贝的原数组
+                //1 :从哪个位置拷贝
+                //mHits:拷贝的目标数组
+                //0 : 从目标数组的那个位置开始写
+                //mHits.length-1:拷贝的长度
+                System.arraycopy(mHits, 1, mHits, 0, mHits.length-1);
+                mHits[mHits.length-1] = SystemClock.uptimeMillis();//将离开机的时间设置给数组的第二个元素，离开机时间：毫秒值，手机休眠不算
+                if (mHits[0] >= (SystemClock.uptimeMillis()-500)) {//判断是否是多击操作
+                    System.out.println("双击了。。。。");
+                    //双击居中
+                    int l = (width - ll_dragview_toast.getWidth())/2;
+                    int t = (height - 25 -ll_dragview_toast.getHeight())/2;
+                    ll_dragview_toast.layout(l,t,l+ll_dragview_toast.getWidth(),t+ll_dragview_toast.getHeight());
+                    //保存控件坐标
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putInt("x",l);
+                    editor.putInt("y",t);
+                    editor.commit();
+                }
+            }
+        });
     }
 
     /**
@@ -97,6 +147,17 @@ public class DragViewActivity extends Activity {
                             break;
                         }
                         ll_dragview_toast.layout(l,t,r,b);//重新绘制控件
+                        //判断textView的隐藏显示了
+                        int top = ll_dragview_toast.getTop();
+                        if(top > height/2){
+                            //隐藏下方显示上方
+                            tv_dragview_bottom.setVisibility(View.INVISIBLE);
+                            tv_dragview_top.setVisibility(View.VISIBLE);
+                        }else {
+                            //隐藏上方显下方
+                            tv_dragview_bottom.setVisibility(View.VISIBLE);
+                            tv_dragview_top.setVisibility(View.INVISIBLE);
+                        }
                         //5 更新开始的坐标
                         startX = newX;
                         startY = newY;
@@ -115,8 +176,8 @@ public class DragViewActivity extends Activity {
                     default:
                         break;
                 }
-                //true ： 事件消费了，执行了，false：表示事件被拦截了
-                return true;
+                //true ： 事件消费了，执行了，false：表示事件被拦截了，有点击事件时，是false，没点击事件时，为false
+                return false;
             }
         });
     }
--- app/src/main/java/com/qzl/shoujiweishi/service/AddressServices.java
+++ app/src/main/java/com/qzl/shoujiweishi/service/AddressServices.java
@@ -12,7 +12,9 @@ import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
@@ -27,8 +29,12 @@ public class AddressServices extends Service {
     private WindowManager windowManager;
     private MyOutGoingCallReceiver myOutGoingCallReceiver;
     private TextView textView;
+    private int width;
+    private int height;
     private SharedPreferences sp;
     private View view;
+    private WindowManager.LayoutParams params;
+
     public AddressServices() {
     }
 
@@ -59,6 +65,14 @@ public class AddressServices extends Service {
         //listener：电话状态的回调监听
         // events : 监听电话的事件  LISTEN_NONE : 不做任何监听操作  LISTEN_CALL_STATE : 监听电话状态的操作
         telephonyManager.listen(myPhoneStateListener,PhoneStateListener.LISTEN_CALL_STATE);
+
+        //获取屏幕宽度
+        WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        //windowManager.getDefaultDisplay().getWidth();
+        DisplayMetrics outMetrics = new DisplayMetrics();//创建一张白纸
+        windowManager.getDefaultDisplay().getMetrics(outMetrics);//给白纸设置宽高
+        width = outMetrics.widthPixels;
+        height = outMetrics.heightPixels;
     }
 
     /**
@@ -131,34 +145,112 @@ public class AddressServices extends Service {
         textView.setTextColor(Color.RED);*/
         //3.设置toast的属性
         //layoutparams是toast的属性,控件要添加到那个父控件中,父控件就要使用那个父控件的属性,表示控件的属性规则符合父控件的属性规则
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+        params = new WindowManager.LayoutParams();
         params.height = WindowManager.LayoutParams.WRAP_CONTENT;//高度包裹内容
         params.width = WindowManager.LayoutParams.WRAP_CONTENT; //宽度包裹内容
-        params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE  //没有焦点
-                | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE  // 不可触摸
-                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON; // 保持当前屏幕
+        params.flags =
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE  //没有焦点
+                |
+//                        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE  // 不可触摸
+//                |
+                                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON; // 保持当前屏幕
         params.format = PixelFormat.TRANSLUCENT; // 透明
-        params.type = WindowManager.LayoutParams.TYPE_TOAST; // 执行toast的类型
+        //TYPE_PRIORITY_PHONE:优先于电话的类型
+        params.type = WindowManager.LayoutParams.TYPE_PRIORITY_PHONE; // 使用 TYPE_PHONE  也可以//执行toast的类型,toast天生没有获取焦点和触摸事件的
         //设置toast位置
         params.gravity = Gravity.LEFT | Gravity.TOP;
         params.x = sp.getInt("x",100);//不是坐标，表示边框的距离，根据gravity来设置的，如果gravity是left的话表示距离左边框的距离，如果是right的话，表示距离有边框的距离
         params.y = sp.getInt("y",100);//跟x的含义一样
+
+        setTouch();
         //效果冲突的话，以默认的效果为主
         //params.gravity = Gravity.LEFT | Gravity.RIGHT;
         //2 将view对象添加到windowManager中
         //params : layoutparams  控件的属性
         //将params属性设置给view对象，并添加到WindowManager中
-        windowManager.addView(view,params);//将一个view对象添加到桌面
+        windowManager.addView(view, params);//将一个view对象添加到桌面
+    }
+
+    /**
+     * toast触摸监听事件
+     */
+    private void setTouch() {
+        view.setOnTouchListener(new View.OnTouchListener() {
+            private int startX;
+            private int startY;
+            //v:当前控件
+            //event:控件执行的事件
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //event.getAction() 获取控件执行的事件
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        //按下的事件
+                        System.out.println("按下了。。。。");
+                        //1 按下控件，记录开始的x和y的坐标
+                        startX = (int) event.getRawX();
+                        startY = (int) event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        //移动的事件
+                        System.out.println("移动了。。。");
+                        //2 移动到新的位置，记录新的位置的x和y的坐标
+                        int newX = (int) event.getRawX();
+                        int newY = (int) event.getRawY();
+                        //3 计算偏移量
+                        int dx = newX - startX;
+                        int dy = newY - startY;
+                        //4.移动相应的偏移量，重新绘制控件
+                        params.x += dx;
+                        params.y += dy;
+                        //控制控件的坐标不能移出微博电话界面
+                        if(params.x < 0 ){
+                            params.x = 0;
+                        }
+                        if (params.y < 0){
+                            params.y = 0;
+                        }
+                        if(params.x > width - view.getWidth()){
+                            params.x = width - view.getWidth();
+                        }
+                        if(params.y > width - view.getHeight() - 25){
+                            params.x = width - view.getHeight()-25;
+                        }
+
+                        windowManager.updateViewLayout(view,params);//更新windowManager中的控件的操作
+                        //5 更新开始的坐标
+                        startX = newX;
+                        startY = newY;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        //抬起的事件
+                        System.out.println("抬起了。。。。");
+                        //保存控件的坐标，保存的是控件的坐标，不是手指的坐标
+                        //获取控件坐标
+                        int x = params.x;
+                        int y = params.y;
+                        SharedPreferences.Editor editor = sp.edit();
+                        editor.putInt("x",x);
+                        editor.putInt("y",y);
+                        editor.commit();
+                    default:
+                        break;
+                }
+                //true ： 事件消费了，执行了，false：表示事件被拦截了，有点击事件时，是false，没点击事件时，为false
+                return false;
+            }
+        });
     }
 
     /**
      * 隐藏toast
      */
     public void hideToast(){
-        if(windowManager != null && textView != null){
-            windowManager.removeView(textView);
+        System.out.println("隐藏toast");
+        if(windowManager != null && view != null){
+            windowManager.removeView(view);
             windowManager = null;
-            textView = null;
+            view = null;
         }
     }
 
