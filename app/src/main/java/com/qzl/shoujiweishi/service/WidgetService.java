15b9be5c8a62f07c08dec97b183f47c7ec201dc7
Qzl <2538096489@qq.com>
2016-07-12 17:50:12 +0800

    一键清理 1 在timer中进行操作 2 创建pendingIntent 3 pendingIntent中需要一个Intent，创建Intent设置发送广播事件 4 创建一个清理进程的广播接收者接收广播   a 创建广播接收者   b 注册广播接收者   c 注销广播接收者   d 清理进程 5 关闭服务时，停止更新操作

--- app/src/main/java/com/qzl/shoujiweishi/service/WidgetService.java
+++ app/src/main/java/com/qzl/shoujiweishi/service/WidgetService.java
@@ -1,9 +1,14 @@
 package com.qzl.shoujiweishi.service;
 
+import android.app.ActivityManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.os.SystemClock;
 import android.widget.RemoteViews;
@@ -14,6 +19,7 @@ import com.qzl.shoujiweishi.utils.TaskUtil;
 
 import java.sql.Time;
 import java.util.Formatter;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -23,6 +29,7 @@ import java.util.TimerTask;
 public class WidgetService extends Service {
 
     private AppWidgetManager appWidgetManager;
+    private WidgetReceiver widgetReceiver;
 
     public WidgetService() {
     }
@@ -32,10 +39,46 @@ public class WidgetService extends Service {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
+    /**
+     * 清理进程的广播接受者
+     */
+    private class WidgetReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //清理进程
+            killProcess();
+        }
+    }
+
+    /**
+     * 清理进程的方法
+     */
+    private void killProcess() {
+        ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        //获取正在运行的进程
+        List<ActivityManager.RunningAppProcessInfo> runningAppProcess = am.getRunningAppProcesses();
+        for (ActivityManager.RunningAppProcessInfo runningAppProcessInfo : runningAppProcess) {
+            //判断我们的应用
+            if (!runningAppProcessInfo.processName.equals(getPackageName())) {
+                am.killBackgroundProcesses(runningAppProcessInfo.processName);
+            }
+        }
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
         //更新桌面小控件
+        //注册清理进程广播接受者
+        //1 广播接受者
+        widgetReceiver = new WidgetReceiver();
+        //2 设置接受的广播事件
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("aa.bb.cc");
+        //注册广播接受者
+        registerReceiver(widgetReceiver,intentFilter);
+
+
         //widget的管理者
         appWidgetManager = AppWidgetManager.getInstance(this);
         //更新操作
@@ -78,9 +121,28 @@ public class WidgetService extends Service {
                 // text:更新的内容
                 views.setTextViewText(R.id.process_count,"正在运行软件："+ TaskUtil.getProcessCount(WidgetService.this));
                 views.setTextViewText(R.id.process_memory,"可用内存："+ android.text.format.Formatter.formatFileSize(WidgetService.this,TaskUtil.getAcailableRam(WidgetService.this)));
+                //按钮点击事件
+                Intent intent = new Intent();
+                intent.setAction("aa.bb.cc");//设置要发送的广播，aa.bb.cc：是自定义的广播事件
+                //通过发送一个广播表示要执行清理操作，通过接受发送的广播执行清理的操作
+                //flags：指定信息的标签  FLAG_UPDATE_CURRENT:更新我们当前的操作
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(WidgetService.this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+                // viewID:点击的控件ID
+                // pendingIntent:延迟意图 包含了一个intent意图，当点击的时候才去执行这个意图，不点击就不执行意图
+                views.setOnClickPendingIntent(R.id.btn_clear,pendingIntent);
                 //更新操作
                 appWidgetManager.updateAppWidget(provider,views);
             }
         }, 2000, 2000);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //注销清理进程的广播接受者
+        if(widgetReceiver != null){
+            unregisterReceiver(widgetReceiver);
+            widgetReceiver = null;
+        }
     }
 }
