de850067fc12cf979415888191b481a92aa115b0
Qzl <2538096489@qq.com>
2016-07-13 21:23:26 +0800

    解锁的操作 1 通过发送广播的方式，去给服务发送解锁信息 2 在服务中通过注册广播的形式实现接收广播的信息，并进行解锁操作 加锁的操作：锁屏之后进行重新加锁 1 注册锁屏的广播接收者 2 在锁屏的广播接收者中将报名设置为null，表示加锁操作

--- app/src/main/java/com/qzl/shoujiweishi/WatchDogActivity.java
+++ app/src/main/java/com/qzl/shoujiweishi/WatchDogActivity.java
@@ -7,14 +7,18 @@ import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class WatchDogActivity extends AppCompatActivity {
 
     private ImageView iv_watchdog_icon;
     private TextView tv_watchdog_name;
     private String packageName;
+    private EditText et_watchdog_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,7 @@ public class WatchDogActivity extends AppCompatActivity {
         setContentView(R.layout.activity_watch_dog);
         iv_watchdog_icon = (ImageView) findViewById(R.id.iv_watchdog_icon);
         tv_watchdog_name = (TextView) findViewById(R.id.tv_watchdog_name);
+        et_watchdog_password = (EditText) findViewById(R.id.et_watchdog_password);
         //接收获取数据
         Intent intent = getIntent();
         packageName = intent.getStringExtra("packageName");
@@ -42,7 +47,6 @@ public class WatchDogActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-
     }
 
     @Override
@@ -68,5 +72,28 @@ public class WatchDogActivity extends AppCompatActivity {
             finish();
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * 解锁点击事件
+     * @param view
+     */
+    public void lock(View view) {
+        //解锁
+        // 1 获取输入的密码
+        String passeword = et_watchdog_password.getText().toString().trim();
+        // 2 判断密码输入是否正确
+        if("123".equals(passeword)){
+            //解锁
+            //一般通过广播的形式将信息发送给服务
+            Intent intent = new Intent();
+            intent.setAction("com.qzl.shoujiweishi.unlock");//自定义要发送的广播事件
+            intent.putExtra("packageName",packageName);
+            sendBroadcast(intent);
+            finish();
+
+        }else {
+            Toast.makeText(getApplicationContext(), "密码错误", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/qzl/shoujiweishi/service/WatchDogService.java
+++ app/src/main/java/com/qzl/shoujiweishi/service/WatchDogService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningTaskInfo;
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
@@ -18,6 +20,9 @@ public class WatchDogService extends Service {
 
 	private WatchDogDao watchDogDao;
 	private boolean isLock;
+	private UnLockCurrentReceiver unLockCurrentReceiver;
+	private String unlockcurrentPackagName;
+	private ScreenOffReceiver screenOffReceiver;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -27,6 +32,21 @@ public class WatchDogService extends Service {
 	public void onCreate() {
 		super.onCreate();
 		watchDogDao = new WatchDogDao(getApplicationContext());
+		//注册解锁的广播接收者
+		// 1 创建一个广播接收者
+		unLockCurrentReceiver = new UnLockCurrentReceiver();
+		// 2 设置接收广播事件
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction("com.qzl.shoujiweishi.unlock");
+		//3 注册广播接收者
+		registerReceiver(unLockCurrentReceiver,intentFilter);
+
+		//注册锁屏的广播接受者和
+		screenOffReceiver = new ScreenOffReceiver();
+		IntentFilter screenOffIntentFilter = new IntentFilter();
+		screenOffIntentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+		registerReceiver(screenOffReceiver,screenOffIntentFilter);
+
 		//时时刻刻监听用户打开的程序
 		//activity都是存放在任务栈中的,一个应用只有一个任务栈
 		final ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
@@ -50,12 +70,13 @@ public class WatchDogService extends Service {
 						System.out.println(packageName);
 						//通过查询数据库，如果数据库中有，弹出解锁界面，没有，不做操作
 						if (watchDogDao.queryLockApp(packageName)) {
-							System.out.println("存在加锁程序");
-							//弹出解锁界面
-							Intent intent = new Intent(WatchDogService.this, WatchDogActivity.class);
-							intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-							intent.putExtra("packageName",packageName);
-							startActivity(intent);
+							if (!packageName.equals(unlockcurrentPackagName)){
+								//弹出解锁界面
+								Intent intent = new Intent(WatchDogService.this, WatchDogActivity.class);
+								intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+								intent.putExtra("packageName",packageName);
+								startActivity(intent);
+							}
 						}
 					}
 					System.out.println("----------------------");
@@ -65,10 +86,39 @@ public class WatchDogService extends Service {
 		}.start();
 	}
 
+	/**
+	 * 解锁广播接收者
+	 */
+	private class UnLockCurrentReceiver extends BroadcastReceiver{
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			//解锁操作
+			unlockcurrentPackagName = intent.getStringExtra("packageName");
+		}
+	}
+
+	private class ScreenOffReceiver extends BroadcastReceiver{
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			//加锁的操作
+			unlockcurrentPackagName = null;
+		}
+	}
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		//当服务关闭的时候我们要去禁止监听
 		isLock = false;
+		//注销解锁的广播接受者和
+		if (unLockCurrentReceiver != null){
+			unregisterReceiver(unLockCurrentReceiver);
+			unLockCurrentReceiver = null;
+		}
+		//注销锁屏的广播接收者
+		if (screenOffReceiver != null){
+			unregisterReceiver(screenOffReceiver);
+			screenOffReceiver = null;
+		}
 	}
 }
