a45b02c50cb18aba2e166629eff031df9fb1027c
Qzl <2538096489@qq.com>
2016-07-15 19:19:05 +0800

    缓存清理扫描操作

--- app/src/main/java/com/qzl/shoujiweishi/fragment/CacheFragment.java
+++ app/src/main/java/com/qzl/shoujiweishi/fragment/CacheFragment.java
@@ -1,19 +1,29 @@
 package com.qzl.shoujiweishi.fragment;
 
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.qzl.shoujiweishi.R;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CacheFragment extends Fragment {
+
+    private TextView tv_cachefragment_text;
+    private ProgressBar pb_cachefragment_progressbar;
 
     public CacheFragment() {
         // Required empty public constructor
@@ -42,6 +52,8 @@ public class CacheFragment extends Fragment {
         //参数二：容器
         //参数三：自动挂载，一律false
         View view = inflater.inflate(R.layout.fragment_cache, container, false);
+        tv_cachefragment_text = (TextView) view.findViewById(R.id.tv_cachefragment_text);
+        pb_cachefragment_progressbar = (ProgressBar) view.findViewById(R.id.pb_cachefragment_progressbar);
         return view;
     }
 
@@ -52,5 +64,52 @@ public class CacheFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        scanner();
+    }
+
+    /**
+     * 扫描的操作
+     */
+    private void scanner() {
+        // 1 获取包的管理者
+        //getActivity:获取fragment所在的activity
+        final PackageManager pm = getActivity().getPackageManager();
+        tv_cachefragment_text.setText("正在初始化128核扫描引擎");
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                SystemClock.sleep(100);
+                List<PackageInfo> installedPackages = pm.getInstalledPackages(0);
+                //设置进度条最大进度
+                pb_cachefragment_progressbar.setMax(installedPackages.size());
+                int count = 0;
+                for (PackageInfo packageInfo : installedPackages) {
+                    SystemClock.sleep(100);
+                    //设置进度条最大进度和当前进度
+                    count++;
+                    pb_cachefragment_progressbar.setProgress(count);
+                    //设置扫描显示的应用名称
+                    final String name = packageInfo.applicationInfo.loadLabel(pm).toString();
+                    if (getActivity() != null) {
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tv_cachefragment_text.setText("正在扫描：" + name);
+                            }
+                        });
+                    }
+                }
+                if (getActivity() != null){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tv_cachefragment_text.setVisibility(View.GONE);
+                            pb_cachefragment_progressbar.setVisibility(View.GONE);
+                        }
+                    });
+                }
+            }
+        }.start();
     }
 }
